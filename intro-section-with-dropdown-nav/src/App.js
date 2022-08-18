efe9af44fea58daf38b0fc1b3c791390d9dc6ab6
enochlee <enochakinbode466@gmail.com>
2022-08-18 16:08:31 -0700

    initialization

---
+++ intro-section-with-dropdown-nav/src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
