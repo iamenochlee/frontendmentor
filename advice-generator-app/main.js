cc1ed9e34c04ab9dff870171e1a237207bd17031
enochlee <enochakinbode466@gmail.com>
2022-08-27 15:33:42 -0700

    used async method

--- advice-generator-app/main.js
+++ advice-generator-app/main.js
@@ -18,3 +18,46 @@ function getData() {
 getData();
 
 adviceBtn.addEventListener('click', getData);
+
+
+
+// const adviceBtn = document.querySelector('#advice-generator');
+// const adviceNumber = document.querySelector('#advice-no');
+// const  adviceEl = document.querySelector('#advice');
+
+
+// const adviceGenerator = (resource) => {
+//     return new Promise((resolve, reject) => {
+//         const request = new XMLHttpRequest();
+
+//         request.addEventListener('readystatechange', () => {
+            
+//             if (request.readyState === 4 && request.status === 200) {
+//                 const data = JSON.parse(request.responseText);
+//                 resolve(data)
+//             } else if(request.readyState === 4) {
+//                 reject("error getting resource")
+//             }
+//         })
+
+//         request.open('GET', resource);
+//         request.send();
+//     })
+// }
+
+
+// const getAdvice = async () => {
+//     await adviceGenerator(`https://api.adviceslip.com/advice/${Math.floor(Math.random() * 200)}`)
+//     .then(data => {
+//         adviceEl.innerHTML = `“${data.slip.advice}”`;
+//         adviceNumber.innerHTML = `ADVICE #${data.slip.id}`;
+//     })
+//     .catch(err => console.log(err))
+    
+// }
+
+// getAdvice();
+
+// adviceBtn.addEventListener('click', getAdvice);
+
+
