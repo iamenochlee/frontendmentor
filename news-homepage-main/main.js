f8806bee62df4c5624de6d9b093c44f3e08d35b7
enochlee <enochakinbode466@gmail.com>
2022-10-29 16:23:49 +0100

    major changes

--- news-homepage-main/main.js
+++ news-homepage-main/main.js
@@ -1,47 +1,60 @@
 const toggler = document.querySelector(".menu-toggler");
-const navBar = document.querySelector(".list--container");
-const body = document.body;
-const image = toggler.querySelector("img");
+const navEl = document.querySelector("nav");
+const icon = toggler.querySelector("img");
 const lists = document.querySelectorAll("li");
+const sections = document.querySelectorAll(".section");
 
 //opening navbar and closing navbar
-const handleOpen = (el, closeEl) => {
+const handleOpen = (el) => {
   el.style.display = "block";
-  closeEl.src = "./assets/images/icon-menu-close.svg";
-  closeEl.alt = "Close Navigation Menu";
-  body.style.overflow = "hidden";
+  icon.src = "./assets/images/icon-menu-close.svg";
+  icon.alt = "Close Navigation Menu";
+  document.body.style.overflow = "hidden";
 };
 
-const handleClose = (el, closeEl) => {
+const handleClose = (el) => {
   el.style.display = "none";
-  closeEl.alt = "Open Navigation menu";
-  body.style.overflow = "scroll";
-  closeEl.src = "./assets/images/icon-menu.svg";
+  icon.alt = "Open Navigation menu";
+  icon.src = "./assets/images/icon-menu.svg";
+  document.body.style.overflowY = "scroll";
 };
 
+//toggling navBar
 toggler.addEventListener("click", () => {
-  if (navBar.style.display === "block") {
-    handleClose(navBar, image);
-  } else if ((navBar.style.display = "none")) {
-    handleOpen(navBar, image);
+  if (navEl.style.display === "block") {
+    handleClose(navEl);
+  } else if (navEl.style.display === "none") {
+    handleOpen(navEl);
   }
 });
 
 document.addEventListener("click", () => {
   if (
-    navBar !== document.activeElement &&
-    body.getBoundingClientRect().width < 720
+    navEl !== document.activeElement &&
+    document.body.getBoundingClientRect().width < 720
   ) {
     if (document.activeElement !== toggler) {
-      handleClose(navBar, image);
+      handleClose(navEl);
     }
   }
 });
 
-//animation
+//animations
 
 lists.forEach((list, i) => {
   list.style.animationDuration = `${
     i === 0 || 1 ? 0.3 + i / 5 : i / 4 - i / 30
   }s`;
+});
+
+sections.forEach((section) => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const intersecting = entry.isIntersecting;
+      intersecting
+        ? section.classList.add("animate")
+        : section.classList.remove("animate");
+    });
+  });
+  observer.observe(section);
 });
