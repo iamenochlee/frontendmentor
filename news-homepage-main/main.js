16da572beeca427435f60dbeed9bb126666b9a4f
enochlee <enochakinbode466@gmail.com>
2022-11-04 12:12:15 +0100

    fixed navbar and missing font

--- news-homepage-main/main.js
+++ news-homepage-main/main.js
@@ -5,44 +5,44 @@ const lists = document.querySelectorAll("li");
 const sections = document.querySelectorAll(".section");
 const body = document.body;
 
-//opening navbar and closing navbar
-const handleOpen = (el) => {
-  el.style.display = "block";
-  icon.src = "./assets/images/icon-menu-close.svg";
-  icon.alt = "Close Navigation Menu";
-  body.style.overflow = "hidden";
-};
+//custom functions
+function handleClose(el) {
+  el.classList.remove("open");
+}
 
-const handleClose = (el) => {
-  el.style.display = "none";
-  icon.alt = "Open Navigation menu";
-  icon.src = "./assets/images/icon-menu.svg";
-  body.style.overflowY = "scroll";
-};
+function handleIcon() {
+  if (toggler.classList.contains("open")) {
+    icon.src = "./public/assets/images/icon-menu-close.svg";
+    icon.alt = "Close Navigation Menu";
+  } else if (!toggler.classList.contains("open")) {
+    icon.src = "./public/assets/images/icon-menu.svg";
+    icon.alt = "Open Navigation Menu";
+  }
+}
 
 //toggling navBar
-navEl.tabIndex = 0;
 toggler.addEventListener("click", () => {
-  if (navEl.style.display === "none") {
-    handleOpen(navEl);
-  } else if (navEl.style.display !== "none") {
-    handleClose(navEl);
-  }
+  toggler.classList.toggle("open");
+  navEl.classList.toggle("open");
+  handleIcon();
 });
 
+//closing on click outside
+navEl.tabIndex = 0;
 document.body.addEventListener("click", () => {
   if (document.activeElement !== toggler) {
     if (
       navEl !== document.activeElement &&
       document.body.getBoundingClientRect().width < 720
     ) {
+      toggler.classList.remove("open");
       handleClose(navEl);
+      handleIcon();
     }
   }
 });
 
 //animations
-
 lists.forEach((list, i) => {
   list.style.animationDuration = `${
     i === 0 || 1 ? 0.3 + i / 5 : i / 4 - i / 30
@@ -53,9 +53,7 @@ sections.forEach((section) => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       const intersecting = entry.isIntersecting;
-      intersecting
-        ? section.classList.add("animate")
-        : section.classList.remove("animate");
+      intersecting && section.classList.add("animate");
     });
   });
   observer.observe(section);
